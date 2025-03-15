208f4f03af6ddb42ee1e6836bfeac7f4827a70ca
ehroy <anjaykoko69@gmail.com>
2025-03-16 01:10:10 +0700

    fix update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,31 +2,24 @@
 <html lang="en" class="scroll-smooth">
 <head>
     @php $setting = \App\Helpers\SettingsHelper::getSettings(); @endphp
-    <meta charset="utf-8">
+    <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Favicon (Gunakan WebP atau SVG untuk lebih ringan) -->
-    <link rel="icon" type="image/svg+xml" href="{{ url('/storage/'.$setting['icon']) }}">
-
-    <!-- Vite untuk Laravel & Vue -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'], 'build')
-
-    <!-- Preload dan Optimasi Font -->
+   <link rel="shortcut icon" href="{{url('/storage/'.$setting['icon'])}}"/>
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
+    <link rel="preload" href="https://cdn.jsdelivr.net/npm/@mdi/font/css/materialdesignicons.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
+    <noscript><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font/css/materialdesignicons.min.css"></noscript>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="preload" href="https://fonts.gstatic.com/s/roboto/v30/KFOmCnqEu92Fr1Mu4mxK.woff2" as="font" type="font/woff2" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" media="print" onload="this.onload=null;this.media='all';">
-    <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"></noscript>
-
-    <!-- Optimasi Material Design Icons -->
-    <link rel="preload" href="https://cdn.jsdelivr.net/npm/@mdi/font/css/materialdesignicons.min.css" as="style">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font/css/materialdesignicons.min.css" media="print" onload="this.onload=null;this.media='all';">
-    <noscript><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font/css/materialdesignicons.min.css"></noscript>
-
-    <!-- Inertia.js Head -->
+    <link rel="preload" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
+    <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"></noscript>    <style type="text/css">
+    .poppins-black,.poppins-bold,.poppins-extrabold,.poppins-extralight,.poppins-light,.poppins-medium,.poppins-regular,.poppins-semibold,.poppins-thin{font-family:Poppins,sans-serif;font-style:normal}.poppins-thin{font-weight:100}.poppins-extralight{font-weight:200}.poppins-light{font-weight:300}.poppins-regular{font-weight:400}.poppins-medium{font-weight:500}.poppins-semibold{font-weight:600}.poppins-bold{font-weight:700}.poppins-extrabold{font-weight:800}.poppins-black{font-weight:900}.poppins-black-italic,.poppins-bold-italic,.poppins-extrabold-italic,.poppins-extralight-italic,.poppins-light-italic,.poppins-medium-italic,.poppins-regular-italic,.poppins-semibold-italic,.poppins-thin-italic{font-family:Poppins,sans-serif;font-style:italic}.poppins-thin-italic{font-weight:100}.poppins-extralight-italic{font-weight:200}.poppins-light-italic{font-weight:300}.poppins-regular-italic{font-weight:400}.poppins-medium-italic{font-weight:500}.poppins-semibold-italic{font-weight:600}.poppins-bold-italic{font-weight:700}.poppins-extrabold-italic{font-weight:800}.poppins-black-italic{font-weight:900}
+    </style>
     @inertiaHead
 </head>
+
 <body>
     @inertia
 </body>
+
 </html>
