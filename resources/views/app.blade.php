b6f1464265b197ffc51ed5354d19ddd7a9b2f787
ehroy <anjaykoko69@gmail.com>
2025-03-23 02:56:12 +0700

    update fix panel

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,27 +1,19 @@
 <!DOCTYPE html>
-<html lang="id">
+<html lang="en" class="scroll-smooth">
 <head>
     @php $setting = \App\Helpers\SettingsHelper::getSettings(); @endphp
-    <meta charset="utf-8">
+    <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">    
-    <!-- Favicon dari Storage -->
-    <link rel="shortcut icon" href="{{ url('/storage/' . $setting['icon']) }}" type="image/png">
-
-    <!-- Load Tailwind & Custom CSS -->
+   <link rel="shortcut icon" href="{{url('/storage/'.$setting['icon'])}}"/>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-    <!-- Material Design Icons (Subset) -->
     <link rel="preload" href="https://cdn.jsdelivr.net/npm/@mdi/font/css/materialdesignicons.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
     <noscript><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font/css/materialdesignicons.min.css"></noscript>
-
-    <!-- Optimized Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&family=Poppins:wght@100;300;400;500;600;700;900&display=swap">
-
-
-
+    <link rel="preload" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
+    <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"></noscript>    <style type="text/css">
+    .poppins-black,.poppins-bold,.poppins-extrabold,.poppins-extralight,.poppins-light,.poppins-medium,.poppins-regular,.poppins-semibold,.poppins-thin{font-family:Poppins,sans-serif;font-style:normal}.poppins-thin{font-weight:100}.poppins-extralight{font-weight:200}.poppins-light{font-weight:300}.poppins-regular{font-weight:400}.poppins-medium{font-weight:500}.poppins-semibold{font-weight:600}.poppins-bold{font-weight:700}.poppins-extrabold{font-weight:800}.poppins-black{font-weight:900}.poppins-black-italic,.poppins-bold-italic,.poppins-extrabold-italic,.poppins-extralight-italic,.poppins-light-italic,.poppins-medium-italic,.poppins-regular-italic,.poppins-semibold-italic,.poppins-thin-italic{font-family:Poppins,sans-serif;font-style:italic}.poppins-thin-italic{font-weight:100}.poppins-extralight-italic{font-weight:200}.poppins-light-italic{font-weight:300}.poppins-regular-italic{font-weight:400}.poppins-medium-italic{font-weight:500}.poppins-semibold-italic{font-weight:600}.poppins-bold-italic{font-weight:700}.poppins-extrabold-italic{font-weight:800}.poppins-black-italic{font-weight:900}
+    </style>
     @inertiaHead
 </head>
 
